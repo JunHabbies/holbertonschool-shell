4709dc7aa0780fc7102601b73e7d2a3c276266ac
JunHabbies <9553@holbertonstudents.com>
2024-06-11 09:02:10 +0000

    test

--- script_push.sh
+++ script_push.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Vérifie si un message de commit a été fourni en argument
 if [ -z "$1" ]; then
   echo "Usage: $0 'message de commit'"
   exit 1
 fi
-
 git add .
 git commit -m "$1"
 git push

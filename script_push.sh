974d88dd2bd4ce5fde076b1ee3ccd48d0dee7c61
JunHabbies <9553@holbertonstudents.com>
2024-06-11 07:44:14 +0000

    test

--- script_push.sh
+++ script_push.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-# Ce script ajoute, commit et pousse les modifications vers le dépôt Git
-
 # Vérifie si un message de commit a été fourni en argument
 if [ -z "$1" ]; then
   echo "Usage: $0 'message de commit'"
   exit 1
 fi
 
-# Ajouter tous les changements
 git add .
-
-# Faire un commit avec le message fourni
 git commit -m "$1"
-
-# Pousser les modifications vers le dépôt distant
 git push
 
